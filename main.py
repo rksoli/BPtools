f642f484c4da7b1766f7730d12e5498b2718edba
rakoso <rakosoliver827@gmail.com>
2020-09-16 14:03:59 +0200

    empty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from BPtools import *
 
 my_model = BPModule()
+data_loader = None  # TODO: dataloader kimásolása a training base-ből a dataloading.py-ba
 Trainer = BPTrainer(model=my_model)
+Trainer.fit(model=my_model)
 print(my_model.parameters())
 print(Trainer.model.parameters())
