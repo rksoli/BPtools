4918b3e64d9e7c91ab079abc19b2907d987f8acf
Oliver Rakos <rakosoliver827@gmail.com>
2020-09-28 15:42:57 +0200

    data loading

---
+++ BPtools/trainer/connetcors/model_connector.py
@@ -0,0 +1,9 @@
+class ModelConnector:
+    def __init__(self, trainer):
+        self.trainer = trainer
+
+    def connect(self, model):
+        model.trainer = self.trainer
+
+    def get_model(self):
+        return self.trainer.model
