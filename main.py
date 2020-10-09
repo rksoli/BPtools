22c7fc1e7fba0df568e6d72bcaf2cd27d071689f
Oliver Rakos <rakosoliver827@gmail.com>
2020-10-09 17:10:48 +0200

    empty

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ encoder = VarEncoderConv1d(2, 60, 10)
 decoder = VarDecoderConv1d_3(2, 60, 10)
 my_model = VariationalAutoEncoder(encoder, decoder)
 data_loader = None  # TODO: dataloader kimásolása a training base-ből a dataloading.py-ba
-Trainer = BPTrainer(epochs=80000, criterion=CustomLossVAE(0.1, 1.0))
+Trainer = BPTrainer(epochs=20000, criterion=CustomLossVAE(0.1, 1.0))
 print(isinstance(my_model, BPModule))
 Trainer.fit(model=my_model)
 # print(my_model.parameters())
