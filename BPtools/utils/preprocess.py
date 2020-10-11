81ca7fe6e9c831dcf7732d35b12389b6ce214376
rakoso <rakosoliver827@gmail.com>
2020-10-11 23:31:59 +0200

    first commit

---
+++ BPtools/utils/preprocess.py
@@ -0,0 +1,379 @@
+import csv
+import numpy as np
+import pandas as pd
+
+from BPtools.utils.vehicle import *
+from typing import Dict
+
+def preprocess_for_classification(raw_dataset: VehicleDataset, window_size: int, shift: int) -> Dict[str, Trajectories]:
+
+    """
+    :param raw_dataset: Ez egy VehicleDataset osztály, amiben egy listában vannak tárolva a jármű objektumok.
+    :param window_size: A szekvencia hossza
+    :param shift: Az az egész szám, amivel eltova vannak egymáshoz képest a szekvenciák
+    :return: Egy dictionaryvel tér vissza, ami tartalmazza a feature-ök nevét, és a Trajektóriákat. A trajektóriák
+            adat formája: M, N, feature, window_size ahol M a járművek száma, amiből adat lett kinyerve, N az egy
+            jűrműből szármaszó trajektóriák (N = int(window_size / shift)), feature az, hogy hány dimenziós az állapot
+            és window_size a szekvencia hossza.
+
+            return : M, N, feature, window_size
+
+    Bevallom, lehet hogy a shift=5 és windows size=30 paramétereken kívül elhasal, mert úgy rémlik hogy nem teszteltem
+    más értékekre.
+
+    """
+
+    vehicle_objects = raw_dataset.vehicle_objects
+    number = 0
+    number_left = 0
+    number_right = 0
+    left_iter = []
+    right_iter = []
+    keep_iter = []
+    total_idx = 0
+    # number_of_features = 3
+    # features = np.zeros((number_of_features, window_size))
+
+    # New variables for feature extraction
+    delta_X = np.zeros(window_size)
+    X = np.zeros(window_size)
+    Y = np.zeros(window_size)
+    V = np.zeros(window_size)
+    A = np.zeros(window_size)
+
+    # New containers for features
+    N = int(window_size / shift)
+    #
+    for idx, l_vehicle in enumerate(vehicle_objects):
+        if N * window_size > l_vehicle.size:
+            continue
+        # lane_change_idx, labels = lane_change_to_idx(vehicle)
+        if l_vehicle.lane_change_indicator() is None:
+            continue
+        else:
+            lane_change_idx, indicator = l_vehicle.indicator
+        if lane_change_idx > int(N/2) * window_size:
+            # print(vehicle.id)
+            if indicator == 1:
+                number_right += 1
+                right_iter.append(idx)
+            if indicator == -1:
+                number_left += 1
+                left_iter.append(idx)
+        if lane_change_idx == 0:
+            keep_iter.append(idx)
+            number += 1
+
+    raw_dataset.left_iter = left_iter
+    raw_dataset.right_iter = right_iter
+    raw_dataset.keep_iter = keep_iter
+
+    number_of_features = 1
+    dX_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    dX_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    dX_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 1
+    X_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    X_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    X_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 2
+    dX_Y_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    dX_Y_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    dX_Y_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 2
+    X_Y_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    X_Y_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    X_Y_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 3
+    dX_V_A_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    dX_V_A_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    dX_V_A_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    # left_data.shape
+    # keep_data.shape
+    # right_data.shape
+
+    print(len(left_iter), len(keep_iter), len(right_iter))
+    print(number_left, number, number_right)
+
+    for left in left_iter:
+        lane_change_idx, labels = vehicle_objects[left].indicator
+        for k in range(N):
+
+            index = lane_change_idx - 2 * window_size + k * shift + 1
+            delta_X = (vehicle_objects[left].x[index: index + window_size]
+                           - vehicle_objects[left].x[index - 1: index + window_size - 1])
+            X = vehicle_objects[left].x[index: index + window_size]
+            Y = vehicle_objects[left].y[index: index + window_size]
+            V = vehicle_objects[left].v[index: index + window_size]
+            A = vehicle_objects[left].a[index: index + window_size]
+
+            dX_left_data[total_idx] = delta_X
+            X_left_data[total_idx] = X
+            dX_Y_left_data[total_idx] = np.array([delta_X, Y])
+            X_Y_left_data[total_idx] = np.array([X, Y])
+            dX_V_A_left_data[total_idx] = np.array([delta_X, V, A])
+            total_idx += 1
+    # np.savetxt("left0.csv", left_data, delimiter=",")
+    total_idx = 0
+    for right in right_iter:
+        lane_change_idx, labels = vehicle_objects[right].indicator
+        for k in range(N):
+
+            index = lane_change_idx - 2 * window_size + k * shift + 1
+            delta_X = (vehicle_objects[right].x[index: index + window_size]
+                       - vehicle_objects[right].x[index - 1: index + window_size - 1])
+            X = vehicle_objects[right].x[index: index + window_size]
+            Y = vehicle_objects[right].y[index: index + window_size]
+            V = vehicle_objects[right].v[index: index + window_size]
+            A = vehicle_objects[right].a[index: index + window_size]
+
+            dX_right_data[total_idx] = delta_X
+            X_right_data[total_idx] = X
+            dX_Y_right_data[total_idx] = np.array([delta_X, Y])
+            X_Y_right_data[total_idx] = np.array([X, Y])
+            dX_V_A_right_data[total_idx] = np.array([delta_X, V, A])
+            total_idx += 1
+
+    # np.savetxt("right0.csv", right_data, delimiter=",")
+    total_idx = 0
+
+    for keep in keep_iter:
+        lane_change_idx, labels = vehicle_objects[keep].indicator
+        for k in range(N):
+
+            index = lane_change_idx - 2 * window_size + k * shift + 1
+            delta_X = (vehicle_objects[keep].x[index: index + window_size]
+                       - vehicle_objects[keep].x[index - 1: index + window_size - 1])
+            X = vehicle_objects[keep].x[index: index + window_size]
+            Y = vehicle_objects[keep].y[index: index + window_size]
+            V = vehicle_objects[keep].v[index: index + window_size]
+            A = vehicle_objects[keep].a[index: index + window_size]
+
+            dX_keep_data[total_idx] = delta_X
+            X_keep_data[total_idx] = X
+            dX_Y_keep_data[total_idx] = np.array([delta_X, Y])
+            X_Y_keep_data[total_idx] = np.array([X, Y])
+            dX_V_A_keep_data[total_idx] = np.array([delta_X, V, A])
+            total_idx += 1
+
+    # reshape the arrays in order to block the trajectories by vehicles
+    dX_left_data = np.reshape(dX_left_data, (len(left_iter), N, 1, window_size))
+    dX_right_data = np.reshape(dX_right_data, (len(right_iter), N, 1, window_size))
+    dX_keep_data = np.reshape(dX_keep_data, (len(keep_iter), N, 1, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    X_left_data = np.reshape(X_left_data, (len(left_iter), N, 1, window_size))
+    X_right_data = np.reshape(X_right_data, (len(right_iter), N, 1, window_size))
+    X_keep_data = np.reshape(X_keep_data, (len(keep_iter), N, 1, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    dX_Y_left_data = np.reshape(dX_Y_left_data, (len(left_iter), N, 2, window_size))
+    dX_Y_right_data = np.reshape(dX_Y_right_data, (len(right_iter), N, 2, window_size))
+    dX_Y_keep_data = np.reshape(dX_Y_keep_data, (len(keep_iter), N, 2, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    X_Y_left_data = np.reshape(X_Y_left_data, (len(left_iter), N, 2, window_size))
+    X_Y_right_data = np.reshape(X_Y_right_data, (len(right_iter), N, 2, window_size))
+    X_Y_keep_data = np.reshape(X_Y_keep_data, (len(keep_iter), N, 2, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    dX_V_A_left_data = np.reshape(dX_V_A_left_data, (len(left_iter), N, 3, window_size))
+    dX_V_A_right_data = np.reshape(dX_V_A_right_data, (len(right_iter), N, 3, window_size))
+    dX_V_A_keep_data = np.reshape(dX_V_A_keep_data, (len(keep_iter), N, 3, window_size))
+
+    dX_traject = Trajectories(dX_left_data, dX_right_data, dX_keep_data, window_size, shift, featnumb=1)
+    X_traject = Trajectories(X_left_data, X_right_data, X_keep_data, window_size, shift, featnumb=1)
+    dX_Y_traject = Trajectories(dX_Y_left_data, dX_Y_right_data, dX_Y_keep_data, window_size, shift, featnumb=2)
+    X_Y_traject = Trajectories(X_Y_left_data, X_Y_right_data, X_Y_keep_data, window_size, shift, featnumb=2)
+    dX_V_A_traject = Trajectories(dX_V_A_left_data, dX_V_A_right_data, dX_V_A_keep_data, window_size, shift, featnumb=3)
+
+    return {"dX": dX_traject,
+            "X": X_traject,
+            "dX_Y": dX_Y_traject,
+            "X_Y": X_Y_traject,
+            "dX_V_A": dX_V_A_traject}
+
+
+def preprocess_for_coding(raw_dataset: VehicleDataset, window_size: int) -> Dict[str, Trajectories]:
+
+    """
+
+    :param raw_dataset: Ez egy VehicleDataset osztály, amiben egy listában vannak tárolva a jármű objektumok.
+    :param window_size: A szekvencia hossza
+    :return: Egy dictionaryvel tér vissza, ami tartalmazza a feature-ök nevét, és a Trajektóriákat.
+            A trajektóriák
+            adat formája: M, N, feature, window_size ahol M a járművek száma, amiből adat lett kinyerve, N az egy
+            jűrműből szármaszó trajektóriák (N = int(window_size / shift)), feature az, hogy hány dimenziós az állapot
+            és window_size a szekvencia hossza.
+
+            return : M, N, feature, window_size
+    """
+
+    vehicle_objects = raw_dataset.vehicle_objects
+    number = 0
+    number_left = 0
+    number_right = 0
+    left_iter = []
+    right_iter = []
+    keep_iter = []
+    total_idx = 0
+
+    # New variables for feature extraction
+    delta_X = np.zeros(window_size)
+    X = np.zeros(window_size)
+    Y = np.zeros(window_size)
+    V = np.zeros(window_size)
+    A = np.zeros(window_size)
+    # New containers for features
+    N = 2  # because I want one windows size before and after the lane change
+    #
+    for idx, l_vehicle in enumerate(vehicle_objects):
+        if 2 * window_size > l_vehicle.size:
+            continue
+        # lane_change_idx, labels = lane_change_to_idx(vehicle)
+        if l_vehicle.lane_change_indicator() is None:
+            continue
+        else:
+            lane_change_idx, indicator = l_vehicle.indicator
+        # this statement is needed for filtering out the lane changing scenarios which are in one window_size
+        # counted backwards.
+        if lane_change_idx + window_size >= l_vehicle.size:
+            l_vehicle.indicator = None
+            continue
+        if lane_change_idx > window_size:
+            # print(vehicle.id)
+            if indicator == 1:
+                number_right += 1
+                right_iter.append(idx)
+            if indicator == -1:
+                number_left += 1
+                left_iter.append(idx)
+        if lane_change_idx == 0:
+            keep_iter.append(idx)
+            number += 1
+
+    raw_dataset.left_iter = left_iter
+    raw_dataset.right_iter = right_iter
+    raw_dataset.keep_iter = keep_iter
+
+    number_of_features = 1
+    dX_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    dX_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    dX_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 1
+    X_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    X_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    X_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 2
+    dX_Y_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    dX_Y_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    dX_Y_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 2
+    X_Y_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    X_Y_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    X_Y_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    number_of_features = 3
+    dX_V_A_left_data = np.zeros((len(left_iter) * N, number_of_features, window_size))
+    dX_V_A_keep_data = np.zeros((len(keep_iter) * N, number_of_features, window_size))
+    dX_V_A_right_data = np.zeros((len(right_iter) * N, number_of_features, window_size))
+
+    print(len(left_iter), len(keep_iter), len(right_iter))
+    print(number_left, number, number_right)
+
+    for left in left_iter:
+        lane_change_idx, labels = vehicle_objects[left].indicator
+        # print(left)
+        for k in range(N):
+            index = lane_change_idx - window_size + k * window_size + 1
+            delta_X = (vehicle_objects[left].x[index: index + window_size]
+                       - vehicle_objects[left].x[index - 1: index + window_size - 1])
+            X = vehicle_objects[left].x[index: index + window_size]
+            Y = vehicle_objects[left].y[index: index + window_size]
+            V = vehicle_objects[left].v[index: index + window_size]
+            A = vehicle_objects[left].a[index: index + window_size]
+
+            dX_left_data[total_idx] = delta_X
+            X_left_data[total_idx] = X
+            dX_Y_left_data[total_idx] = np.array([delta_X, Y])
+            X_Y_left_data[total_idx] = np.array([X, Y])
+            dX_V_A_left_data[total_idx] = np.array([delta_X, V, A])
+            total_idx += 1
+
+    total_idx = 0
+    for right in right_iter:
+        lane_change_idx, labels = vehicle_objects[right].indicator
+        for k in range(N):
+            index = lane_change_idx - window_size + k * window_size + 1
+            delta_X = (vehicle_objects[right].x[index: index + window_size]
+                       - vehicle_objects[right].x[index - 1: index + window_size - 1])
+            X = vehicle_objects[right].x[index: index + window_size]
+            Y = vehicle_objects[right].y[index: index + window_size]
+            V = vehicle_objects[right].v[index: index + window_size]
+            A = vehicle_objects[right].a[index: index + window_size]
+
+            dX_right_data[total_idx] = delta_X
+            X_right_data[total_idx] = X
+            dX_Y_right_data[total_idx] = np.array([delta_X, Y])
+            X_Y_right_data[total_idx] = np.array([X, Y])
+            dX_V_A_right_data[total_idx] = np.array([delta_X, V, A])
+            total_idx += 1
+
+    total_idx = 0
+    for keep in keep_iter:
+        lane_change_idx, labels = vehicle_objects[keep].indicator
+        lane_change_idx = vehicle_objects[keep].size // 2
+        for k in range(N):
+
+            index = lane_change_idx - window_size + k * window_size + 1
+            delta_X = (vehicle_objects[keep].x[index: index + window_size]
+                       - vehicle_objects[keep].x[index - 1: index + window_size - 1])
+            X = vehicle_objects[keep].x[index: index + window_size]
+            Y = vehicle_objects[keep].y[index: index + window_size]
+            V = vehicle_objects[keep].v[index: index + window_size]
+            A = vehicle_objects[keep].a[index: index + window_size]
+
+            # print(keep, k)
+            dX_keep_data[total_idx] = delta_X
+            X_keep_data[total_idx] = X
+            dX_Y_keep_data[total_idx] = np.array([delta_X, Y])
+            X_Y_keep_data[total_idx] = np.array([X, Y])
+            dX_V_A_keep_data[total_idx] = np.array([delta_X, V, A])
+            total_idx += 1
+
+    # reshape the arrays in order to block the trajectories by vehicles
+    dX_left_data = np.reshape(dX_left_data, (len(left_iter), N, 1, window_size))
+    dX_right_data = np.reshape(dX_right_data, (len(right_iter), N, 1, window_size))
+    dX_keep_data = np.reshape(dX_keep_data, (len(keep_iter), N, 1, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    X_left_data = np.reshape(X_left_data, (len(left_iter), N, 1, window_size))
+    X_right_data = np.reshape(X_right_data, (len(right_iter), N, 1, window_size))
+    X_keep_data = np.reshape(X_keep_data, (len(keep_iter), N, 1, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    dX_Y_left_data = np.reshape(dX_Y_left_data, (len(left_iter), N, 2, window_size))
+    dX_Y_right_data = np.reshape(dX_Y_right_data, (len(right_iter), N, 2, window_size))
+    dX_Y_keep_data = np.reshape(dX_Y_keep_data, (len(keep_iter), N, 2, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    X_Y_left_data = np.reshape(X_Y_left_data, (len(left_iter), N, 2, window_size))
+    X_Y_right_data = np.reshape(X_Y_right_data, (len(right_iter), N, 2, window_size))
+    X_Y_keep_data = np.reshape(X_Y_keep_data, (len(keep_iter), N, 2, window_size))
+    # reshape the arrays in order to block the trajectories by vehicles
+    dX_V_A_left_data = np.reshape(dX_V_A_left_data, (len(left_iter), N, 3, window_size))
+    dX_V_A_right_data = np.reshape(dX_V_A_right_data, (len(right_iter), N, 3, window_size))
+    dX_V_A_keep_data = np.reshape(dX_V_A_keep_data, (len(keep_iter), N, 3, window_size))
+
+    dX_traject = Trajectories(dX_left_data, dX_right_data, dX_keep_data, window_size, shift=window_size/2, featnumb=1)
+    X_traject = Trajectories(X_left_data, X_right_data, X_keep_data, window_size, shift=window_size/2, featnumb=1)
+    dX_Y_traject = Trajectories(dX_Y_left_data, dX_Y_right_data, dX_Y_keep_data, window_size, shift=window_size/2, featnumb=2)
+    X_Y_traject = Trajectories(X_Y_left_data, X_Y_right_data, X_Y_keep_data, window_size, shift=window_size/2, featnumb=2)
+    dX_V_A_traject = Trajectories(dX_V_A_left_data, dX_V_A_right_data, dX_V_A_keep_data, window_size, shift=window_size/2, featnumb=3)
+
+    return {"dX": dX_traject,
+            "X": X_traject,
+            "dX_Y": dX_Y_traject,
+            "X_Y": X_Y_traject,
+            "dX_V_A": dX_V_A_traject}
---
+++ BPtools/utils/vehicle.py
@@ -0,0 +1,244 @@
+from __future__ import annotations
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+feetToMeters = lambda feet: float(feet) * 0.3048
+converters_dict = {'Local_X': feetToMeters,
+                   'Local_Y': feetToMeters,
+                   'v_length': feetToMeters,
+                   'v_Width': feetToMeters,
+                   'v_Vel': feetToMeters,
+                   'v_Acc': feetToMeters,
+                   'Space_Headway': feetToMeters}
+
+
+class VehicleDataset(Dataset):
+    """NGSIM vehicle dataset"""
+
+    def __init__(self, csv_file, root_dir=None, transform=None):
+        """
+        asd
+        """
+        self.all_data = pd.read_csv(csv_file, delimiter=',', header=0)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.vehicle_objects = None
+        self.left_iter = None
+        self.right_iter = None
+        self.keep_iter = None
+
+    def __len__(self):
+        """returns with all the number of frames of the dataset"""
+        return len(self.vehicle_objects)
+
+    def __getitem__(self, idx):
+        """returns the idx_th vehicle"""
+        return self.vehicle_objects[idx]
+
+    def create_objects(self):
+        i = 0
+        vehicle_objects = []
+        all_data = np.array(self.all_data)
+        while len(all_data) > i:
+            total_frames = int(all_data[i][2])
+            until = i + total_frames
+            data = all_data[i:until]
+            vehicle = VehicleData(data)
+            # vehicle.lane_changing()
+            vehicle_objects.append(vehicle)
+            i = until
+        self.vehicle_objects = vehicle_objects
+
+    def create_vehicle_objects(self):
+        vehicle_objects = []
+        for _, vehicle_id in self.all_data.groupby('Vehicle_ID'):
+            for _, vehicle_id_tf in vehicle_id.groupby('Total_Frames'):
+                vehicle = VehicleData(np.array(vehicle_id_tf))
+                vehicle_objects.append(vehicle)
+        self.vehicle_objects = vehicle_objects
+
+
+class Trajectories(Dataset):
+
+    def __init__(self, left=None, right=None, keep=None, window_size=None, shift=None, featnumb=None,
+                 csv_file=None, root_dir=None, transform=None, data=None, dataset=None, labels=None):
+
+        if csv_file is not None:
+            self.all_data = np.array(pd.read_csv(csv_file, delimiter=',', header=0))
+        else:
+            self.all_data = data
+        self.left = left
+        self.right = right
+        self.keep = keep
+        self.window_size = window_size
+        self.shift = shift
+        self.featnumb = featnumb
+        self.root_dir = common.FULLDATA_PATH
+        self.transform = transform
+        self.vehicle_objects = None
+        self.dataset = dataset
+        self.labels = labels
+
+
+    def __len__(self):
+        """returns with a trajectory sample"""
+        return self.dataset.shape[0]
+
+    def __getitem__(self, idx):
+        """returns with a trajectory sample"""
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        sample = {'data': self.dataset[idx], 'label': self.labels[idx]}
+        if self.transform is not None:
+            sample = self.transform(sample)
+        return sample
+
+    def __add__(self, other: Trajectories):
+
+        assert ((self.window_size == other.window_size) & (self.shift == other.shift))
+        left = np.concatenate((self.left, other.left), axis=0)
+        right = np.concatenate((self.right, other.right), axis=0)
+        keep = np.concatenate((self.keep, other.keep), axis=0)
+        return Trajectories(left, right, keep, self.window_size, self.shift)
+
+    def __iadd__(self, other: Trajectories):
+        assert ((self.window_size == other.window_size) & (self.shift == other.shift))
+        self.left = np.concatenate((self.left, other.left), axis=0)
+        self.right = np.concatenate((self.right, other.right), axis=0)
+        self.keep = np.concatenate((self.keep, other.keep), axis=0)
+        return self
+
+    def create_dataset(self):
+        left_size = self.left.shape[0]
+        right_size = self.right.shape[0]
+        keep_size = self.keep.shape[0]
+        min_size = np.min((left_size, right_size, keep_size))
+        np.random.seed(seed=420)
+        conc = np.concatenate((self.left[np.random.choice(left_size, min_size, replace=False), :],
+                               self.right[np.random.choice(right_size, min_size, replace=False), :],
+                               self.keep[np.random.choice(keep_size, min_size, replace=False), :]),
+                              axis=0)
+
+        np.random.seed(seed=911)
+        self.dataset = conc[np.random.permutation(conc.shape[0])]
+        self.create_labels(min_size)
+
+    def create_labels(self, size: int):
+        l = np.array([1, 0, 0])
+        k = np.array([0, 1, 0])
+        r = np.array([0, 0, 1])
+
+        N = int(self.window_size / self.shift)
+        size = N * size
+        labels = np.zeros((size * 3, 3))
+        for i in range(0, size):
+            labels[i] = l
+            labels[i + size] = r
+            labels[i + 2 * size] = k
+
+        labels = np.reshape(labels, (-1, N, 3))
+        np.random.seed(seed=911)
+        self.labels = labels[np.random.permutation(labels.shape[0])]
+
+    def write_csv(self):
+        path = self.root_dir
+        np.savetxt(path + "left.csv", self.left, delimiter=",")
+        np.savetxt(path + "right.csv", self.right, delimiter=",")
+        np.savetxt(path + "keep.csv", self.keep, delimiter=",")
+
+    def save_np_array(self):
+        path = self.root_dir
+        np.save(path + 'left.npy', self.left)
+        np.save(path + 'right.npy', self.right)
+        np.save(path + 'keep.npy', self.keep)
+
+    def save_np_dataset_labels(self, name, mode=None):
+        path = self.root_dir
+        if mode is not None:
+            name = name + mode
+        np.save(path + "/" + name + '_dataset.npy', self.dataset)
+        np.save(path + "/" + name + '_labels.npy', self.labels)
+
+
+class VehicleData:
+
+    def __init__(self, data):
+        # car ID
+        self.id = int(data[0, 0])
+        # frame ID
+        self.frames = data[:, 1]
+        # total frame number
+        self.size = int(data[0, 2])
+        # global time
+        self.t = data[:, 3]
+        # lateral x coordinate
+        self.x = data[:, 4]
+        # Longitudinal y coordinate
+        self.y = data[:, 5]
+        # Dimensions of the car: Length, Width
+        self.dims = data[0, 8:10]
+        # Type, 1-motor, 2-car, 3-truck
+        self.type = int(data[0, 10])
+        # Instantenous velocity
+        self.v = data[:, 11]
+        # Instantenous acceleration
+        self.a = data[:, 12]
+        # lane ID: 1 is the FARTHEST LEFT. 5 is the FARTHEST RIGHT.
+        # 6 is Auxiliary lane for off and on ramp
+        # 7 is on ramp
+        # 8 is off ramp
+        self.lane_id = data[:, 13]
+        # [None] if no lane change; [+/-1, frame] if there is a lane change in the specific frame
+        # [0, frame_id] or [-1, frame_id] or [1, frame_id]
+        self.change_lane = None
+        # mean, variance, changes or not?, frame id
+        self.labels = None
+
+        self.indicator = None
+
+    def __getitem__(self, frame_number):
+        item = []
+        # returns a numpy array with features corresponding to a specific frame number. The first frame is the zeroth.
+        return item
+
+    def set_change_lane(self, l_change):
+        self.change_lane = l_change
+
+    def lane_changing(self):
+        l_change = []
+        total_frames = self.size
+
+        for i in range(int(total_frames) - 1):
+            if (self.lane_id[i + 1] - self.lane_id[i]) != 0:
+                l_change.append([self.lane_id[i + 1] - self.lane_id[i],
+                                 self.frames[i + 1]])
+            else:
+                l_change.append([0, self.frames[i + 1]])
+        l_change = np.array(l_change)
+        self.set_change_lane(l_change)
+
+    def lane_change_indicator(self):
+        j = 0
+        indicator = 0
+        index = 0
+
+        while (j < self.size -1) & (index == 0):
+            difference = self.lane_id[j + 1] - self.lane_id[j]
+            if difference != 0:
+                index = j
+                indicator = difference
+            j = j + 1
+        self.indicator = [index, indicator]
+        return self.indicator
+
+
+class ToDevice(object):
+    def __call__(self, sample):
+        # print(sample['data'].shape)
+        sample = {'data': torch.from_numpy(sample['data'].transpose((1, 0))).float().to(device),
+                  'label': torch.from_numpy(sample['label']).to(device=device, dtype=torch.float)}
+        return sample
