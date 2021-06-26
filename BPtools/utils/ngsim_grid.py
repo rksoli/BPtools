6db9575bc3b4ca2ee7c0115ef095530fd3a1e32c
rakoso <rakosoliver827@gmail.com>
2021-06-26 13:03:47 +0200

    final

--- BPtools/utils/ngsim_grid.py
+++ BPtools/utils/ngsim_grid.py
@@ -187,7 +187,8 @@ class OccupancyGrid:
                 # Todo(Data): itt az ego-ból kellene kinyerni a trajektóriát
                 print("\tEgo ID: ", ego_v_ID)
                 print("\tframes: ", ego.size)
-                ego_traj = np.concatenate((ego.x.reshape(-1,1), ego.y.reshape(-1,1), ego.lane_id.reshape(-1,1)), axis=1)
+                ego_traj = np.concatenate((ego.x.reshape(-1,1), ego.y.reshape(-1,1), ego.v.reshape(-1,1),
+                                           ego.lane_id.reshape(-1,1)), axis=1)
                 # for ego_lane_ID, group_ego_v_lane in group_ego_vehicles.groupby("Lane_ID"):
                 #     print("\t\tlane: ", ego_lane_ID)
                 # print(ego_traj.shape)
