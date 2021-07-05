2725767495af40fea66df8912f3073e2cb891eac
rakoso <rakosoliver827@gmail.com>
2021-07-05 15:54:44 +0200

    small changes

--- BPtools/utils/ngsim_grid.py
+++ BPtools/utils/ngsim_grid.py
@@ -91,11 +91,11 @@ class OccupancyGrid:
         # idősor csoportosítás kezdő időpont szerint.
         for T, group_time_start in self.data.groupby("Time_Start"):
             print("T: ", T)
-            tj=0
-            if T == 75649:
-                pass
-            else:
-                continue
+            # tj=0
+            # if T == 75649:
+            #     pass
+            # else:
+            #     continue
 
             data_T = []
             i = 1
@@ -166,13 +166,13 @@ class OccupancyGrid:
                 print("\tVehicles: ", len(data_T))
 
                 if len(data_T) == 100:
-                    np.save(str(T) + '_' + str(i), np.array(data_T))
+                    np.save('D:/dataset/us-101/' + str(T) + '_' + str(i), np.array(data_T))
                     data_T = []
                     i = i + 1
                 # ha nem érte el a 100-at, ki kéne még menteni
             # maradék data_T kimentése, ha van
             if len(data_T) > 0:
-                np.save(str(T) + '_' + str(i), np.array(data_T))
+                np.save('D:/dataset/us-101/' + str(T) + '_' + str(i), np.array(data_T))
 
     def trajectory_for_grid(self):
         self.data["Time_Start"] = self.data["Global_Time"] / 100 - self.data["Frame_ID"] - 1.11343e+10
@@ -233,6 +233,6 @@ if __name__ == "__main__":
     # results = client.get("8ect-6jqj")#, limit=2000)
     # results_df = pd.DataFrame.from_records(results)
     # print(results_df.head())
-    grid = OccupancyGrid(csv_file_name='../../../full_data/i-80.csv', deltaX=0.5, deltaY=0.5)
+    grid = OccupancyGrid(csv_file_name='../../../full_data/us-101.csv', deltaX=0.5, deltaY=0.5)
     grid.grid_data_for_ae()
     # grid.trajectory_for_grid()
